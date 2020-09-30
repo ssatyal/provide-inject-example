05d8f67c39917b94b636243885a59a92dc3d98dd
Sanjaya Satyal <Sanjaya-Satyal@ip-192-168-86-187.eu-west-1.compute.internal>
2020-09-30 15:47:46 -0400

    add prop drill example

---
+++ src/components/nest-one.vue
@@ -0,0 +1,16 @@
+<template>
+  <nest-two :msg="msg"/>
+</template>
+
+<script>
+import NestTwo from './nest-two.vue';
+export default {
+  name: 'NestOne',
+  components: {
+    NestTwo,
+  },
+  props: {
+    msg: String
+  }
+}
+</script>
